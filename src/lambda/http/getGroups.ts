5232c95c5c5e006ff8780c61e55be989dc187f72
Farouq Umar <ufakz99@gmail.com>
2022-07-19 18:15:39 +0100

    init

---
+++ src/lambda/http/getGroups.ts
@@ -0,0 +1,30 @@
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import 'source-map-support/register'
+import * as AWS from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const groupsTable = process.env.GROUPS_TABLE
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    console.log('Processing event: ', event)
+
+    const result = await docClient.scan(
+        {
+            TableName: groupsTable
+        }
+    ).promise()
+
+    const items = result.Items
+
+    // Return result
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            items
+        })
+    }
+}
