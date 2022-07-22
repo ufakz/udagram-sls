6c5002cb0f5d73983707a835db56e388f032f5b6
Farouq Umar <ufakz99@gmail.com>
2022-07-22 23:29:37 +0100

    feat: use middy middleware

--- src/lambda/auth/auth0Authorizer.ts
+++ src/lambda/auth/auth0Authorizer.ts
@@ -1,22 +1,21 @@
-import { CustomAuthorizerEvent, CustomAuthorizerResult, CustomAuthorizerHandler } from "aws-lambda";
+import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
-import * as AWS from 'aws-sdk'
-import { verify } from 'jsonwebtoken'
+import * as middy from 'middy'
+import { secretsManager } from 'middy/middlewares'
 
-import { JwtToken } from "../../auth/JwtToken";
+import { verify } from 'jsonwebtoken'
+import { JwtToken } from '../../auth/JwtToken'
 
 const secretId = process.env.AUTH_0_SECRET_ID
 const secretField = process.env.AUTH_0_SECRET_FIELD
 
-const client = new AWS.SecretsManager()
-
-// Cache secret if a Lambda instance is reused
-let cachedSecret: string
-
-export const handler: CustomAuthorizerHandler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
+export const handler = middy(async (event: CustomAuthorizerEvent, context): Promise<CustomAuthorizerResult> => {
     try {
-        const decodedToken = await verifyToken(event.authorizationToken)
-        console.log('User was verified')
+        const decodedToken = verifyToken(
+            event.authorizationToken,
+            context.AUTH0_SECRET[secretField]
+        )
+        console.log('User was authorized', decodedToken)
 
         return {
             principalId: decodedToken.sub,
@@ -37,7 +36,7 @@ export const handler: CustomAuthorizerHandler = async (event: CustomAuthorizerEv
         return {
             principalId: 'user',
             policyDocument: {
-                Version: "2012-10-17",
+                Version: '2012-10-17',
                 Statement: [
                     {
                         Action: 'execute-api:Invoke',
@@ -47,37 +46,30 @@ export const handler: CustomAuthorizerHandler = async (event: CustomAuthorizerEv
                 ]
             }
         }
-
-
     }
+})
 
-}
-
-async function verifyToken(authHeader: string): Promise<JwtToken> {
+function verifyToken(authHeader: string, secret: string): JwtToken {
     if (!authHeader)
-        throw new Error('No authorization header')
+        throw new Error('No authentication header')
 
-
-    if (!authHeader.toLocaleLowerCase().startsWith('bearer '))
-        throw new Error('Invalid authorization header')
+    if (!authHeader.toLowerCase().startsWith('bearer '))
+        throw new Error('Invalid authentication header')
 
     const split = authHeader.split(' ')
     const token = split[1]
 
-    const secretObject: any = await getSecret()
-    const secret = secretObject[secretField]
-
-    return verify(token,secret) as JwtToken
+    return verify(token, secret) as JwtToken
 }
 
-async function getSecret() {
-    if (cachedSecret) return cachedSecret
-
-    const data = await client.getSecretValue({
-        SecretId: secretId
-    }).promise()
-
-    cachedSecret = data.SecretString
-
-    return JSON.parse(cachedSecret)
-}
+handler.use(
+    secretsManager({
+        cache: true,
+        cacheExpiryInMillis: 60000,
+        // Throw an error if can't read the secret
+        throwOnFailedCall: true,
+        secrets: {
+            AUTH0_SECRET: secretId
+        }
+    })
+)
--- src/lambda/http/createImage.ts
+++ src/lambda/http/createImage.ts
@@ -3,6 +3,9 @@ import 'source-map-support/register'
 import * as AWS from 'aws-sdk'
 import * as uuid from 'uuid'
 
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+
 const docClient = new AWS.DynamoDB.DocumentClient()
 const s3 = new AWS.S3({
   signatureVersion: 'v4'
@@ -13,53 +16,48 @@ const imagesTable = process.env.IMAGES_TABLE
 const bucketName = process.env.IMAGES_S3_BUCKET
 const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  console.log('Caller event', event)
-  const groupId = event.pathParameters.groupId
-  const validGroupId = await groupExists(groupId)
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    console.log('Caller event', event)
+    const groupId = event.pathParameters.groupId
+    const validGroupId = await groupExists(groupId)
 
-  if (!validGroupId) {
+    if (!validGroupId) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          error: 'Group does not exist'
+        })
+      }
+    }
+
+    const imageId = uuid.v4()
+    const timestamp = new Date().toISOString()
+    const parsedBody = JSON.parse(event.body)
+
+    const newImage = {
+      imageId,
+      timestamp,
+      groupId,
+      ...parsedBody,
+      imageUrl: `https://${bucketName}.s3.amazonaws.com/${imageId}`
+    }
+
+    await docClient.put({
+      TableName: imagesTable,
+      Item: newImage
+    }).promise()
+
+    const url = getUploadUrl(imageId)
+
     return {
-      statusCode: 404,
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
+      statusCode: 201,
       body: JSON.stringify({
-        error: 'Group does not exist'
+        newImage: newImage,
+        uploadUrl: url
       })
     }
-  }
-
-  const imageId = uuid.v4()
-  const timestamp = new Date().toISOString()
-  const parsedBody = JSON.parse(event.body)
-
-  const newImage = {
-    imageId,
-    timestamp,
-    groupId,
-    ...parsedBody,
-    imageUrl: `https://${bucketName}.s3.amazonaws.com/${imageId}`
-  }
-
-  await docClient.put({
-    TableName: imagesTable,
-    Item: newImage
-  }).promise()
-
-  const url = getUploadUrl(imageId)
-
-  return {
-    statusCode: 201,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-      newImage: newImage,
-      uploadUrl: url
-    })
-  }
-}
+  })
 
 function getUploadUrl(imageId: string) {
   return s3.getSignedUrl('putObject', {
@@ -82,3 +80,9 @@ async function groupExists(groupId: string) {
   console.log('Get group: ', result)
   return !!result.Item
 }
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
